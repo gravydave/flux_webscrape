d5ae2d733135c5cbdde7280f00be7cd84c566fcb
gravydave <heracuo@gmail.com>
2018-10-23 18:15:08 +0800

    removed useless code and added comments

--- scrape.js
+++ scrape.js
@@ -1,14 +1,18 @@
+/*
+*The following functionality needs to be added or finished:
+*
+*   metaData parsing from HTML
+*   file IO
+*   pagination
+*/
+
 //DEPENDENCIES
-const request = require('request');
 const cheerio = require('cheerio');
 const axios = require('axios');
 
 //URL
 const link = 'http://www.aph.gov.au/Parliamentary_Business/Bills_Legislation/Bills_before_the_House_of_Representatives?drt=2&drv=7&drvH=7&pnu=45&pnuH=45&f=30%2f08%2f2016&to=10%2f09%2f2018&ps=10&ito=1&q=&bs=1&pbh=1&bhor=1&pmb=1&g=1&st=1&sr=1';
 
-//PARAMS
-let pageCounter = 0;
-let resultCount = 0;
 
 const getWebsiteContent = async (link) => 
 {
@@ -17,22 +21,29 @@ const getWebsiteContent = async (link) =>
         const response = await axios.get(link)
         const $ = cheerio.load(response.data)
         var rawBills = $('.search-filter-results').children();
-        var metaData = [];
         
         rawBills.map((i, el) => 
         {
-            const count = resultCount++
             const shortName = $(el).find('h4')
+            const metaData = $(el).find('dl').children() //The <dd> tags contain all relevant data except shortName
             
-            /*
-            *Need to cycle through the <dd> tags held in DL
-            * CUrrently not working at all
-            */
-            const billURL = $(el).find('dl').children()
+            //UNIMPLEMENTED
+            //needs to be extracted from metaData
+            const billDescript = null;
+            const billURL = null;
+            const billDate = null;
             
-            
-            
-            console.log(billURL.text());
+            console.log(shortName.text());
+            console.log(metaData.text());
+
+            //Bill object to be written to .json
+            const billData =
+            {
+                shortName: shortName,
+                billDescript: billDescript,
+                billURL: billURL,
+                billDate: billDate
+            }
         })
 
         
@@ -46,13 +57,3 @@ const getWebsiteContent = async (link) =>
   
 
 getWebsiteContent(link);
-
-
-
-
-//REDUNTANT
-function sleep(seconds)
-{
-    var e = new Date().getTime() + (seconds * 1000);
-    while (new Date().getTime() <= e) {}
-}
